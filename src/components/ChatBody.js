1af97dd0eab1c0541b360573bdfc33f2d4e23126
Carolina Haggerty <carolina.vhaggerty@gmail.com>
2023-05-31 11:42:43 -0700

    practice added board to chat

---
+++ src/components/ChatBody.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { useState , useEffect} from 'react'
+import { io } from "socket.io-client";
+import Board from './Board'
+const server_url = 'http://localhost:4000/'
+
+const socket = io(server_url);
+
+
+function ChatBody(){
+    const styleBoard={
+        border:'2px',
+        borderColor:'black'
+    }
+    console.log(socket)
+    const[roomId,setRoom] =useState('');
+    const [userName, setUsername] = useState('');
+    const[message,setSendMessage] =useState(''); 
+    const[messageReceived, setMessageReceived] = useState([]);
+
+    const handleJoin =(e)=>{
+        e.preventDefault();
+        if(roomId !== ''){
+            socket.emit("join-room",roomId)
+        }
+    };
+    const sendMessage = (e) =>{
+        e.preventDefault()
+        if (message !==''){
+            const messageData ={
+                room:roomId,
+                sender:userName,
+                message:message
+            };
+        socket.emit("send-message",messageData);
+        setMessageReceived((list)=> [...list,messageData]);
+        setSendMessage('');
+        }
+    };
+
+    useEffect(()=>{
+        socket.on('receive-message',(data)=>{
+            setMessageReceived((list)=> [...list,data]);
+        });
+    },[socket])
+
+    return (
+        <div>
+            <h1>Message: </h1>
+            {messageReceived.map((item)=>{
+                return (
+                    <div key={item.sender} id={userName===item.sender?'sender':'receiver'}>
+                    <div>{item.message}</div>
+                    <p>--{item.sender}</p>
+                    </div>
+
+                )
+                
+                
+            })}
+            <input 
+            type="text" 
+            placeholder='userName'
+            name='userName' 
+            onChange={(e)=>{
+                setUsername(e.target.value)
+            }}/>
+            <input
+            type="text" 
+            placeholder='room name'
+            name='roomId' 
+            value={roomId}
+            onChange={(e)=>{
+                setRoom(e.target.value)
+            }}/>
+            <button onClick={handleJoin} >Join</button>
+            <input 
+            type="text"
+             name='message' 
+             value={message}
+             placeholder='message'
+             onChange={(e)=>{
+                setSendMessage(e.target.value)
+            }}/>
+            <button type='submit'  onClick={sendMessage}>send</button>
+            <div>--------------------------------------</div>
+            <div style={styleBoard}>
+            <Board socket={socket}/>
+
+            </div>
+            
+
+        </div>
+    );
+    }
+    
+
+
+
+export default ChatBody
+
